f397822271e35453016b1bd158dd1ccf44f0a13b
Pruthvik Sheth <pruthvik.sheth@avestatechnologies.com>
2024-02-23 17:06:26 +0530

    Init Commit

---
+++ datagenerator/datagenerator.py
@@ -0,0 +1,15 @@
+
+
+class DataGenerator:
+
+    def __init__(self) -> None:
+        pass
+
+    def download_image():
+        pass
+
+    def filter_image():
+        pass
+
+    def infer():
+        pass
