9b9069d0cd8abb2280a868eb4557a90d982cc0e1
Rahil-07 <rahil.mungara@avestatechnologies.com>
2024-02-28 14:30:58 +0530

    notebook updated

--- main.py
+++ main.py
@@ -5,7 +5,17 @@ def main():
     generator = DataGenerator()
     csv_data = generator.load_csv("./data/raw/listings_images.csv")
     print(csv_data.head(10))
+    data_dict = generator.load_raw_data(csv_data)
+
+    for data in data_dict:
+
+
 
 
 if __name__ == "__main__":
     main()
+
+
+
+for index,data in enumerate(data_dict):
+    
