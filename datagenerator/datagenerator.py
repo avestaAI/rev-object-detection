3b87ac7ad3e68e05b876163946fb9ec4585f6564
Pruthvik Sheth <pruthvik.sheth@avestatechnologies.com>
2024-02-29 12:44:36 +0530

    Major Exception handling changes

--- datagenerator/datagenerator.py
+++ datagenerator/datagenerator.py
@@ -1,8 +1,10 @@
 import torch
 import pandas as pd
 import requests as req
+from requests.exceptions import ConnectionError
 import io
 from PIL import Image
+from PIL import UnidentifiedImageError
 import torch
 import os
 import json
@@ -41,22 +43,19 @@ class DataGenerator:
 
     @staticmethod
     def download_image(image_link):
-        try:
-            image_content = req.get(image_link, timeout = 10).content
-            image_file = io.BytesIO(image_content)
-            pil_image = Image.open(image_file)
-            print("-------------------------------------------------------------------------------------------------------")
-            print(f"Downloaded: {image_link}")
-        except Exception as e:
-            print("An error occurred while downloading the image!", e)
-
+        image_content = req.get(image_link, timeout = 10).content
+        image_file = io.BytesIO(image_content)
+        pil_image = Image.open(image_file)
+        print("-------------------------------------------------------------------------------------------------------")
+        print(f"Downloaded: {image_link}")
         return pil_image
-    
 
+    
     def infer(self, image):
         results = self.model(image)
         return eval(results.pandas().xyxy[0].to_json(orient='records'))
     
+
     @staticmethod
     def save_image(image, image_path, image_name):
         if not os.path.exists(image_path):
@@ -67,91 +66,110 @@ class DataGenerator:
             print(f"Saved: {image_name}.jpg")
 
 
+    @staticmethod
     def save_and_filter_image(image, image_info):
-        try:
-            base_path = './data/interim'
+        base_path = './data/interim'
 
-            all_images_path = f"{base_path}/all"
-            DataGenerator.save_image(image=image, image_path=all_images_path, image_name = image_info["listingImageId"])
+        all_images_path = f"{base_path}/all"
+        DataGenerator.save_image(image=image, image_path=all_images_path, image_name = image_info["listingImageId"])
 
-            if len(image_info['predictions']) == 0:
-                return 
-        
-            elif len(image_info['predictions']) == 1:
-                category = image_info['predictions'][0]['name']
-                full_path = f"{base_path}/{category}"
-                image_name = f"{category}_{image_info['listingImageId']}"
-                DataGenerator.save_image(image=image, image_path=full_path, image_name=image_name)
+        if len(image_info['predictions']) == 0:
+            full_path = f"{base_path}/no_predictions"
+            image_name = f"{image_info['listingImageId']}"
+            DataGenerator.save_image(image=image,image_path=full_path,image_name=image_name)
+    
+        elif len(image_info['predictions']) == 1:
+            category = image_info['predictions'][0]['name']
+            full_path = f"{base_path}/{category}"
+            image_name = f"{category}_{image_info['listingImageId']}"
+            DataGenerator.save_image(image=image, image_path=full_path, image_name=image_name)
 
-            else:
-                category = "_".join([prediction['name'] for prediction in image_info['predictions']])
-                full_path = f"{base_path}/mix"
-                image_name = f"{category}_{image_info['listingImageId']}"
-                DataGenerator.save_image(image=image, image_path=full_path, image_name=image_name)
+        else:
+            if not os.path.exists(f"{base_path}/combinations"):
+                os.mkdir(f"{base_path}/combinations")
+
+            category = "_".join([prediction['name'] for prediction in image_info['predictions']])
+            image_name = f"{category}_{image_info['listingImageId']}"
+            full_path = f"{base_path}/combinations/{category}"
+            DataGenerator.save_image(image=image, image_path=full_path, image_name=image_name)
                     
-        except Exception as e:
-            print(f"Error while saving the image : {e}")
 
     @staticmethod
     def save_to_json(data_dict):
-        try:
+        if not os.path.exists("./data/interim/info"):
+            os.mkdir("./data/interim/info")
 
-            if not os.path.exists("./data/interim/info"):
-                os.mkdir("./data/interim/info")
+        file_full_path = "./data/interim/info/images_info.json"
 
-            file_full_path = "./data/interim/info/images_info.json"
+        if not os.path.exists(file_full_path):
+            with open(file_full_path, "w") as outfile: 
+                outfile.write(json.dumps(data_dict, indent=2))
+                print("\n\nRecords saved to JSON!!\n\n")
+        else:
+            with open(file_full_path, "r") as fp:
+                existing_data_dict = json.load(fp)
+                existing_data_dict.extend(data_dict)
 
-            if not os.path.exists(file_full_path):
-                with open(file_full_path, "w") as outfile: 
-                    outfile.write(json.dumps(data_dict, indent=2))
-                    print("\n\nRecords saved to JSON!!")
-            else:
-                with open(file_full_path, "r") as fp:
-                    existing_data_dict = json.load(fp)
-                    existing_data_dict.extend(data_dict)
-
-                with open(file_full_path, "w") as outfile: 
-                    outfile.write(json.dumps(existing_data_dict, indent=2))
-                    print("\n\nRecords saved to JSON!!")
-        except Exception as e:
-            print("Error saving json file!", e)
+            with open(file_full_path, "w") as outfile: 
+                outfile.write(json.dumps(existing_data_dict, indent=2))
+                print("\n\nRecords saved to JSON!!\n\n")
 
 
-    def download_and_infer(self, data_dict, start, end, save_interval):
+    def download_and_infer(self, data_dict, start, end, save_interval, sleep_interval):
         try:
             index = 0
+            failed_images = 0
             start_time = time.time()
             while index < len(data_dict):
-                image_link = data_dict[index]['imageLink']
-                image = DataGenerator.download_image(image_link)
 
-                if len(image.getbands()) <= 3:
-                    results = self.infer(image)
-                    data_dict[index]['predictions'] = results
-                    data_dict[index]['imageNumber'] = start + index
-                    print("Image Number:", start + index)
+                try:
+                    image_link = data_dict[index]['imageLink']
+                    image = DataGenerator.download_image(image_link)
+                    if len(image.getbands()) <= 3:
+                        results = self.infer(image)
+                        data_dict[index]['predictions'] = results
+                        data_dict[index]['imageNumber'] = start + index
+                        print("Image Number:", start + index)
 
-                    DataGenerator.save_and_filter_image(image, data_dict[index])
-                
-                else:
-                    print("An image with alpha channel found, hence discarding it!")
+                        DataGenerator.save_and_filter_image(image, data_dict[index])
+                    
+                    else:
+                        print("An image with alpha channel found, hence discarding it!")
+                        data_dict.pop(index)
+                        continue
+
+                except ConnectionError as e:
+                    print("Connection error, retrying after 10 seconds")
+                    time.sleep(10)
+                    continue
+                except UnidentifiedImageError as e:
+                    print("Image not found in the given link!")
+                    failed_images += 1
                     data_dict.pop(index)
                     continue
-                
+                except Exception as e:
+                    print("Something went wrong!")
+                    failed_images += 1
+                    data_dict.pop(index)
+                    continue
+
+
                 if (index + 1) % save_interval == 0:
                     DataGenerator.save_to_json(data_dict[index+1-save_interval:index+1])
-                    print("Sleeping...\n\n")
+
+                if (index + 1) % sleep_interval == 0:
+                    print("\n\nSleeping...\n\n")
                     time.sleep(4)
 
                 index += 1
 
-            remainder = index % save_interval
 
+            remainder = index % save_interval
             if remainder > 0:
                 DataGenerator.save_to_json(data_dict[-remainder:])
 
             end_time = time.time()
             print(f"Total time elapsed: {(end_time - start_time) / 60} mins")
+            print(f"Total images: {end - start + 1} | Succeded Images: {(end - start + 1) - failed_images} | Failed images: {failed_images}")
         except KeyboardInterrupt:
             DataGenerator.save_to_json(data_dict[index - save_interval:index])
-        # print(data_dict)
--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from datagenerator.datagenerator import DataGenerator
 
 
 def main():
-    start = 31
-    end = 50
-    save_interval = 8
+    start = 491
+    end = 520
+    save_interval = 5
+    sleep_interval = 5
     generator = DataGenerator()
     df = generator.load_csv("./data/raw/listings_images.csv")
     data_dict = generator.load_raw_data(df, start, end)
-    generator.download_and_infer(data_dict, start, end, save_interval)
+    generator.download_and_infer(data_dict, start, end, save_interval, sleep_interval)
     
 
 if __name__ == "__main__":
