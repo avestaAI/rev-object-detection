21b7542c545763bd3a556c8ed0941012e9455e2c
Rahil Mungara <rahil.mungara@avestatechnologies.com>
2024-03-20 17:52:35 +0530

    naming_convention changed

--- data_generator/imagefilterer.py
+++ data_generator/imagefilterer.py
@@ -3,6 +3,8 @@ class ImageFilterer:
 
     def __init__(self, base_path) -> None:
         self.base_path = base_path
+        # Please check spelling of category !!!!!!!!!
+        self.base_categories = ['swimming_pool','dishwasher','fireplace','garage','tennis_court']  
 
     def filter_image(self, image_information):
         num_predictions = len(image_information["predictions"])
@@ -14,10 +16,19 @@ class ImageFilterer:
         elif num_predictions == 1:
             category = image_information['predictions'][0]['name']
             destination_path = f"{self.base_path}/{category}"
-            image_name = f"{category}_{image_id}"
+            image_name = image_id
         else:
-            categories = "_".join([prediction['name'] for prediction in image_information['predictions']])
-            image_name = f"{categories}_{image_id}"
-            destination_path = f"{self.base_path}/combinations/{categories}"
+            flag = 0
+            prediction_categories = [prediction['name'] for prediction in image_information['predictions']]
+            for i in self.base_categories:
+                if i in prediction_categories:
+                    flag = 1
+            
+            # categories = "_".join([prediction['name'] for prediction in image_information['predictions']])
+            image_name = image_id
+            if flag:
+                destination_path = f"{self.base_path}/combinations"
+            else:
+                destination_path = f"{self.base_path}/other_combinations"
 
         return destination_path, image_name
